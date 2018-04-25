35fff3861daff89b4f9ceb47fe570d7eb8731826
Unknown <krzysztof.wierzbicki.wk@gmail.com>
2018-04-25 14:13:07 +0200

    Added extension methods

---
+++ Zadanie3/Zadanie3/ListExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    public static class ListExtensions
+    {
+        public static List<Product> NotCategorized(this List<Product> list)
+        {
+            return new List<Product>(list.Where(p => p.ProductSubcategoryID == null));
+        }
+    }
+}
---
+++ Zadanie3/Zadanie3Test/ListExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zadanie3;
+using System.Linq;
+
+namespace Zadanie3Test
+{
+    [TestClass]
+    public class ListExtensionsTests
+    {
+        static ProductionDataContext dataContext;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            dataContext = new ProductionDataContext();
+        }
+
+        public List<Product> GetProducts()
+        {
+            return new List<Product>(from product in dataContext.Product
+                                     select product);
+        }
+
+        [TestMethod]
+        public void NotCategorizedTest()
+        {
+            var list = GetProducts();
+            list = list.NotCategorized();
+            foreach(var p in list)
+            {
+                Assert.AreEqual(null, p.ProductSubcategoryID);
+            }
+        }
+    }
+}
