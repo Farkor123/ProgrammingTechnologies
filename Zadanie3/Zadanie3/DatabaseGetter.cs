cf428edb15c810918f7b1e7033ba3ecb5ec63f75
Unknown <a.sadowski1@onet.eu>
2018-05-09 21:27:05 +0200

    Rest of 3 sheit

--- Zadanie3/Zadanie3/DatabaseGetter.cs
+++ Zadanie3/Zadanie3/DatabaseGetter.cs
@@ -47,5 +47,56 @@ namespace Zadanie3
 
             return new List<string>(names);
         }
+
+        public static string GetProductVendorByProductName(string productName)
+        {
+            try
+            {
+                return (from p in dataContext.Product
+                        from v in dataContext.ProductVendor
+                        where p.ProductID == v.ProductID && p.Name == productName
+                        select v.Vendor.Name).First();
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public static List<Product> GetProductsWithNRecentReviews(int howManyReviews)
+        {
+            return (from r in dataContext.ProductReview
+                    orderby r.ModifiedDate
+                    select r.Product).Distinct().Take(howManyReviews).ToList();
+        }
+
+        public static List<Product> GetNRecentlyReviewedProducts(int howManyProducts)
+        {
+            return (from r in dataContext.ProductReview
+                    orderby r.ModifiedDate
+                    select r.Product).Distinct().Take(howManyProducts).ToList();
+        }
+
+        public static List<Product> GetNProductsFromCategory(string categoryName, int n)
+        {
+            try
+            {
+                return (from p in dataContext.Product
+                        where p.ProductSubcategory.ProductCategory.Name == categoryName
+                        orderby p.Name
+                        select p).Take(n).ToList();
+            }
+            catch
+            {
+                return new List<Product>();
+            }
+        }
+
+        public static int GetTotalStandardCostByCategory(ProductCategory category)
+        {
+            return (int)(from p in dataContext.Product
+                         where p.ProductSubcategory != null && p.ProductSubcategory.ProductCategory.ProductCategoryID == category.ProductCategoryID
+                         select p).Sum(o => o.StandardCost);
+        }
     }
 }
